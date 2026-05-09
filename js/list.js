c99b78b87f578d59ad5d1d47e77d3699525868f1
Nathaël Noguès <nathael.nogues@gmail.com>
2026-05-09 19:03:36 +0200

    Loading more while playing

--- js/list.js
+++ js/list.js
@@ -16,6 +16,25 @@ function saveToFile() {
 	a.click()
 	URL.revokeObjectURL(url)
 }
+function parse_vid(p) {
+	if(p.length === 11) {
+		return {id: p}
+	} else if(p.length > 11) {
+		id = null
+
+		// Parse yt video URL to video id
+		let match = p.match(/(?:[^/]+\/)*([a-zA-Z0-9_-]{11})/)
+		if(match && match.length > 1)
+			id = match[1]
+		else {
+			// Extract url params, and find parameter "v"
+			match = p.match(/[?&]v=([a-zA-Z0-9_-]{11})/)
+			if(match && match.length > 1) id = match[1]
+		}
+		if(id != null) return {id: id}
+	}
+	return null
+}
 function loadFromList() {
 	// Load from TSV file
 	const input = document.createElement('input')
@@ -28,26 +47,25 @@ function loadFromList() {
 			const newVideoList = []
 			const parts = reader.result.replace(/[ \t\r\n]+/g, ' ').split(' ')
 			for(const p of parts) {
-				if(p.length === 11) {
-					newVideoList.push({id: p})
-				} else if(p.length > 11) {
-					id = null
-
-					// Parse yt video URL to video id
-					let match = p.match(/(?:[^/]+\/)*([a-zA-Z0-9_-]{11})/)
-					if(match && match.length > 1)
-						id = match[1]
-					else {
-						// Extract url params, and find parameter "v"
-						match = p.match(/[?&]v=([a-zA-Z0-9_-]{11})/)
-						if(match && match.length > 1) id = match[1]
-					}
-					if(id != null) newVideoList.push({id: id})
-				}
+				const ldd = parse_vid(p)
+				if(ldd) newVideoList.push(ldd)
 			}
 			loadVideos(newVideoList)
 		}
 		reader.readAsText(file)
 	}
 	input.click()
+}
+function loadFromPopup() {
+	// Promp user for a string
+	const str = prompt('Enter a list of video ids separated by spaces')
+	if(str) {
+		const newVideoList = []
+		const parts = str.replace(/[ \t\r\n]+/g, ' ').split(' ')
+		for(const p of parts) {
+			const ldd = parse_vid(p)
+			if(ldd) newVideoList.push(ldd)
+		}
+		loadVideos(newVideoList)
+	}
 }
